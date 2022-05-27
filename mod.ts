03ad9dfbf0ae0a53b725ff52bd1aad291683c2d3
biewdev <gabrielaugustodev@gmail.com>
2022-05-26 23:40:14 -0300

    chore: Add Middleware

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export * from './src/App.ts';
 export * from './src/Router.ts';
-export type { ResponseCosmic, RequestCosmic, Next } from './src/Router.ts';
+export type { Middleware, Next, RequestCosmic, ResponseCosmic } from './src/Router.ts';
