f012b026378cab751ad1d26dd2bdb2a1bf536a61
biewdev <gabrielaugustodev@gmail.com>
2022-05-29 22:18:52 -0300

    chore: Add App and Router import

--- mod.ts
+++ mod.ts
@@ -1,3 +1,7 @@
 export * from './src/App.ts';
 export * from './src/Router.ts';
+
+export { App } from './src/App.ts';
+export { Router } from './src/Router.ts';
+
 export type { Middleware, Next, RequestCosmic, ResponseCosmic } from './src/Router.ts';
